0ad319b00cca5ac86c6c40c0e2e82cf03a8025f3
Veronika Tasova <vtaseva1@gmail.com>
2024-12-18 19:56:42 +0100

    VT2 JS Loops

--- main.js
+++ main.js
@@ -6,11 +6,60 @@
 
 const exerciseBtn = document.querySelectorAll('.exercise-btn');
 const exercise = document.querySelectorAll('.exercise');
-for (const [i] of exerciseBtn.entries()){
-exerciseBtn[i].addEventListener('click', () => {
-exercise[i].classList.toggle("js-exercise");
-});
-}
+// Excerises open and close (toggle) but they can be seen all open
+
+// for (const [i] of exerciseBtn.entries()){
+// exerciseBtn[i].addEventListener('click', () => {
+// exercise[i].classList.toggle("js-exercise");
+// });
+// }
+
+// Excerises can be open only one at a time but they (toggle) too
+exerciseBtn && exerciseBtn.forEach((el) => {
+  el.addEventListener('click', (e) => {
+    let currentExercise = e.target;
+    // console.log(el)
+    if (currentExercise.innerText == "TITLES") {
+      document.getElementById('titles').classList.toggle("js-exercise");
+      } else {
+        document.getElementById('titles').classList.remove("js-exercise");
+      }
+      if (currentExercise.innerText == "LISTS") {
+      document.getElementById('lists').classList.toggle("js-exercise");
+      } else {
+        document.getElementById('lists').classList.remove("js-exercise");
+      }
+      if (currentExercise.innerText == "TABLE") {
+      document.getElementById('table').classList.toggle("js-exercise");
+      }
+      else {
+        document.getElementById('table').classList.remove("js-exercise");
+      // document.getElementById('titles').classList.remove("js-exercise");
+      // document.getElementById('lists').classList.remove("js-exercise");
+      // document.getElementById('table').classList.remove("js-exercise");
+          }
+
+  })
+})
+
+
+
+// exerciseBtn && exerciseBtn.forEach((el) => {
+//   el.addEventListener('click', (e) => {
+//     let currentExercise = exercise;
+//     exercise.forEach((el) => {
+//       if (el == currentExercise) {
+//         exercise.classList.toggle('js-exercise');
+//       } else {
+//         el.classList.remove('js-exercise');
+//       }
+//     })
+//   })
+// })
+
+
+
+
 
 const studentName = document.querySelectorAll('.student-name');
 const studentSection = document.querySelectorAll('.student-section');
@@ -21,4 +70,20 @@ studentName[i].addEventListener('click', (e) => {
   studentSection[i].classList.add('js-student-section');
   studentName[i].classList.add('js-student-name');
 });
+}
+
+// Changing footer by name
+const studentFooterBtn = document.querySelectorAll('.student-footer-btn');
+const studentFooter = document.querySelectorAll('.student-footer');
+
+for(const [j] of studentFooterBtn.entries()) {
+  // console.log(studentFooterBtn[j].length)
+  // studentFooter[j].style.display ="none";
+  studentFooterBtn[j].addEventListener('click', () => {
+    studentFooter.forEach((student) => student.classList.remove('js-student-footer'));
+    console.log('simona')
+    studentFooter[j].classList.add('js-student-footer');
+    // console.log(studentFooterBtn[j] + "clicked")
+    // studentFooter[j].style.display ="block";
+  })
 }
